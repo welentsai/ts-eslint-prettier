9450f846df08340ab242aedc4f5d065d3794e28e
Tsai WeiLun <welentsai@Tsaidebijixingdiannao.local>
2023-07-02 14:45:57 +0800

    init typescript project template

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const a  = 12
+const b  = 17
+
+const showProduct : (first : number, second : number) => number = (first, second) => {
+    return first * second
+}
+
+console.log(`The product is %d`, showProduct(a,b))
